22b31b3cdae09b008376f5397d855e70f61a4abe
kevin_Arpe <arpeggiogio@gmail.com>
2022-06-10 18:27:15 +0900

    알고리즘 백준 - 정수론, '다리놓기' 풀이 업데이트

--- num_theory/binominal2_solution.js
+++ num_theory/binominal2_solution.js
@@ -1,7 +1,8 @@
-let [N, K] = require('fs').readFileSync('/dev/stdin', 'utf-8').trim().split(' ').map(Number);
+//let [N, K] = require('fs').readFileSync('/dev/stdin', 'utf-8').trim().split(' ').map(Number);
+[N, K] = [29, 13];
 
 function binominalCoefficient(n, k) {
-    const pascalsTriangle = Array.from(new Array(N+1), () => new Array(N+1).fill(0));
+    const pascalsTriangle = Array.from(new Array(n+1), () => new Array(n+1).fill(0));
     /* 파스칼 삼각형 그리기 */
     pascalsTriangle[0][0] = 1;
     pascalsTriangle[1][0] = 1;
@@ -13,17 +14,17 @@ function binominalCoefficient(n, k) {
             else pascalsTriangle[i][j] = pascalsTriangle[i-1][j-1] + pascalsTriangle[i-1][j];
         }
     }
-    return pascalsTriangle[n, k];
+    return pascalsTriangle[n][k];
 }
 
 
 /* 점화식 사용 */
-function binominalCoefficient(n, k) {
-    if (k === 0 || k === n) {
-        return 1;
-    }
-    else {
-        return binominalCoefficient(n-1, k-1) + binominalCoefficient(n-1, k);
-    }
-}
+// function binominalCoefficient(n, k) {
+//     if (k === 0 || k === n) {
+//         return 1;
+//     }
+//     else {
+//         return binominalCoefficient(n-1, k-1) + binominalCoefficient(n-1, k);
+//     }
+// }
 console.log(binominalCoefficient(N, K));
